00166fbbd97103975bcceb1281783acb81032554
Thiago Rocha <throcha3@gmail.com>
2022-05-01 21:30:43 -0300

    conexao ao mongodb remoto

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 //forma de ler JSON
 app.use(
@@ -10,8 +11,21 @@ app.use(
 
 app.use(express.json())
 
-//porta
-app.listen(3000)
+const DB_USER = 'throcha3333'
+const DB_PASSWORD = encodeURIComponent('az3SB5tFDcMylghO')
+
+mongoose
+  .connect(
+    'mongodb+srv://throcha3333:az3SB5tFDcMylghO@apinodejs.wdfgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  )
+  .then(() => {
+    console.log('Conectou ao banco!')
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
+
+//porta 3000
+
 
 //rota ini
 app.get('/', (req,res) =>{
@@ -19,4 +33,3 @@ app.get('/', (req,res) =>{
     res.json({message: 'Oi, Express!'})
 
 })
-
