012f7b7ee216c456fa3cb53243049ac8bce21f8a
Thiago Rocha <throcha3@gmail.com>
2022-05-04 21:01:37 -0300

    delete

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -84,4 +84,27 @@ router.patch('/:id', async (req,res) => {
 
 })
 
+//Delete
+router.delete('/:id', async (req,res) => {
+
+  const id = req.params.id
+
+  const person = await Person.findOne({_id: id})
+
+  if (!person){
+    res.status(422).json({message: 'Person not found!'})
+    return
+  }
+
+  try {
+    await Person.deleteOne({_id: id})
+
+    res.status(200).json({message: 'Person deleted successfully'})
+
+  } catch (error) {
+    res.status(500).json({error: error})
+  }
+
+})
+
 module.exports = router
